464383180719f26fdf84201360d7ea14ded1403f
mmickovska <m_mmickovska@yahoo.com>
2022-05-19 11:37:31 +0200

    Attaching Homework4

---
+++ Desktop/Martina/semos/JAVASCRIPT&CSS-1/js/homework/4/filter-animals.js
@@ -0,0 +1,120 @@
+const addAnimals = () => {
+    const animalType = document.getElementById('animalType')
+    const numOfAnimal = document.getElementById('numOfAnimal')
+  
+    // guard clause
+    if (!animalType.value || !numOfAnimal.value) {
+      alert("Please insert the animal type and the number of specimens")
+      return
+    }
+  
+    // make a new animal object to be inserted in the local storage
+    const newAnimal = {
+      type: animalType.value.toLowerCase(),
+      number: numOfAnimal.value
+    }
+  
+    // check if the animal array exists (when the app is loaded for the first time, the list does NOT exist)
+    let animals = localStorage.getItem('animals')
+    if (!animals) {
+      animals = []
+    } else {
+      // we need to convert the string variable to array in order to push new items
+      animals = JSON.parse(animals)
+    }
+  
+    let animalAlreadyExists = false
+    // proverka dali veke postoi toj tip na zivotno
+    animals = animals.map(animal => {
+      if (animal.type === newAnimal.type) {
+        animalAlreadyExists = true
+        return { ...animal, number: Number(animal.number) + Number(newAnimal.number) }
+      }
+      return animal
+    })
+  
+    // ako ne postoi
+    // add the new animals in the list
+    if (!animalAlreadyExists) {
+      animals = [...animals, newAnimal] // animals.push(newAnimal)
+    }
+  
+    // add  the modified list back into the local storage
+    localStorage.setItem('animals', JSON.stringify(animals))
+  
+    generateListOfAnimalsHtml(animals)
+  }
+  
+  const generateListOfAnimalsHtml = (animalsArray) => {
+    const htmlContainer = document.getElementById('container')
+    // if the UL already exists, remove it and re-generate it again
+    const listOfAnimals = document.getElementById('listOfAnimals')
+    if (listOfAnimals) {
+      listOfAnimals.remove()
+    }
+  
+    const htmlList = document.createElement('ul')
+    htmlList.id = 'listOfAnimals'
+    htmlContainer.appendChild(htmlList)
+  
+    animalsArray.forEach(animal => {
+      const htmlItem = document.createElement('li')
+      htmlItem.textContent = `${animal.type} ${animal.number}`
+      htmlList.appendChild(htmlItem)
+    })
+  }
+  
+  // on application load fetch the animal list from local storage and generate HTML
+  generateListOfAnimalsHtml(JSON.parse(localStorage.getItem('animals')))
+  
+  function search (value) {
+    value = value.toLowerCase().trim()
+    const initialList = JSON.parse(localStorage.getItem('animals'))
+  
+    const filteredList = initialList.filter((animal) => {
+      // String.includes; Array.includes
+      if (animal.type.includes(value)) {
+        return true
+      }
+      return false
+    })
+  
+    /* ALTERNATIVNI RESHENIJA
+    const filteredList = []
+    for (let i = 0; i < initialList.length; i++) {
+      if (initialList[i].type.includes(value)) {
+        filteredList.push(initialList[i])
+      }
+    }
+    
+    for (const animal of intialList) {
+      if (animal.type.includes(value)) {
+        filteredList.push(animal)
+      }
+    }
+    intialList.forEach((animal) => {
+      if (animal.type.includes(value)) {
+        filteredList.push(animal)
+      }
+    })
+    
+    */
+  
+    generateListOfAnimalsHtml(filteredList)
+  }
+  
+  function reset () {
+    document.getElementById('search').value = ''
+    generateListOfAnimalsHtml(JSON.parse(localStorage.getItem('animals')))
+  }
+  
+  
+  function convertStringBackToArray () {
+    ['a', 'b', 'c', 'd'].includes('a')
+  
+    console.log("Hello Semos".split(' '))
+    console.log("Hello Semos".split())
+  
+    ['a', 'b', 'c', 'd'].join('')
+    ['a', 'b', 'c', 'd'].join('%')
+  }
